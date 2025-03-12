6790486bc15b28186f8480a65e9cbfa685c1d391
Alessio <alessio@gmail.com>
2025-03-12 06:48:24 +0100

    first commit prova

---
+++ Documents/NetBeansProjects/Multicast/src/main/java/com/mycompany/multicast/MulticastClient.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.multicast;
+import java.net.*;
+import java.io.*;
+/**
+ *
+ * @author Alessio
+ */
+public class MulticastClient {
+     public static void main(String[] args) throws IOException {
+        byte[] bufferIN = new byte[1024];
+
+        // Parametri del server
+        int porta = 6789;
+        String gruppo = "239.255.0.1"; // Usa un indirizzo multicast valido
+
+        InetAddress group = InetAddress.getByName(gruppo);
+        NetworkInterface netIf = NetworkInterface.getByInetAddress(InetAddress.getLocalHost());
+
+        try (MulticastSocket socket = new MulticastSocket(porta)) {
+            socket.joinGroup(new InetSocketAddress(group, porta), netIf);
+
+            System.out.println("Client in ascolto...");
+
+            while (true) { // Ciclo infinito per ricevere più messaggi
+                DatagramPacket packetIN = new DatagramPacket(bufferIN, bufferIN.length);
+                socket.receive(packetIN);
+
+                String messaggio = new String(packetIN.getData(), 0, packetIN.getLength());
+                System.out.println("Messaggio ricevuto: " + messaggio + " da " + packetIN.getAddress().toString() + " porta: " + packetIN.getPort());
+            }
+
+        } catch (IOException e) {
+            System.err.println("Errore: " + e.getMessage());
+        }
+    }
+}
